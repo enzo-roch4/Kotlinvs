bf8f353ab922efc1c71362d46cfee0f54e1e4ff0
enzo-roch4 <144125363+enzo-roch4@users.noreply.github.com>
2024-08-26 21:09:39 -0300

    add for e while

---
+++ Exercicios/AulaForWhile/Ex.kt
@@ -0,0 +1,93 @@
+//de um a dez
+// fun main (){
+//     var soma = 0
+//     for (i in 1..10) {
+//         println(i)
+//     }
+// }
+
+
+// imprimir multiplos de 0 a 50
+
+// fun main(){
+//     for (i in 0..50 step 5) {
+//         println(i)
+//     }
+// }
+
+
+//calcular numeros naturais
+// fun main(){
+//     val numeros = 10
+//     var soma = 0
+//     for (i in 1..n) {
+//         soma += i
+        
+//     }
+//     println("A soma dos primeiros numeros naturais e de: $soma")
+// }
+
+// fun main (){
+//     var num = 17 
+//     var isPrime = true
+
+//     for (i in 2 until num) {
+//         if (num % i == 0){
+//             isPrime= false
+//             break
+//         }
+//     } 
+    
+//     if(isPrime){
+//         println("$num e um numero primo")
+//     }else{
+//         println("$num nao e um numero primo")
+//     }
+// }
+
+// fun main(){
+//     val word = "kotlin"
+//     for (char in word) {
+//         println(char)
+//     }
+// }
+
+// fun main() {
+//     var lista = arrayOf(8,30,45,43,70,88,)
+//     for (i in lista.indices) {
+//         println(lista[i])
+//     }
+// }
+
+// ex while: contagem regressiva
+
+// fun main (){
+//     println("Digite um numero inteiro positivo")
+//     val input = readLine()!!
+
+//     if(input != null && input.isNotEmpty){
+//         val num = input.toInt()
+
+//     var contador = num
+//     while (contador >= 1 ) {
+//         println(contador)
+//         contador--
+//     }
+// }
+// else{println("invalido")}
+// }
+
+// fun main(){
+
+//     println("Digite um numero inteiro positivo")
+//     val input = readLine()!!.toInt()
+   
+//     var soma = 0
+//     var contador = 3
+//     while(contador >= input) {
+//         soma += contador
+//         contador += 3
+        
+//     }
+//     println("A soma dos primeiros numeros naturais e de: $soma")
+// }
