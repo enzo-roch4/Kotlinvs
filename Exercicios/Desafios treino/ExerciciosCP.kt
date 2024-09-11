0a0a68422c596f5373f16f656dcfb2abae585778
enzo-roch4 <144125363+enzo-roch4@users.noreply.github.com>
2024-09-11 20:31:25 -0300

    Lista de tarefas!

--- Exercicios/Desafios treino/ExerciciosCP.kt
+++ Exercicios/Desafios treino/ExerciciosCP.kt
@@ -39,3 +39,56 @@ fun main(){
 
 }
  */
+
+ class Tarefa(val nome: String, var concluida: Boolean = false)
+fun main() {
+    // Lista 
+    val tarefas = mutableListOf<Tarefa>()
+
+    // Loop para manter o programa executando
+    while (true) {
+        println(" Bem vindo a Lista de tarefas! Opções:")
+        println("1. Adicionar sua tarefa")
+        println("2. Listar todas as tarefas")
+        println("3. Concluir uma tarefa")
+        println("4. Sair")
+
+        print("Escolha uma opção: ")
+        val opcao = readLine()!!.toInt()
+        when (opcao) {
+            1 -> {
+                // Adicionar tarefas
+                print("Digite o nome da tarefa: ")
+                val nomeTarefa = readLine()!!
+                tarefas.add(Tarefa(nomeTarefa))
+                println("Tarefa adicionada com sucesso!")
+            }
+            2 -> {
+                // Listar tarefas
+                println("Tarefas:")
+                tarefas.forEachIndexed { index, tarefa ->
+                    println("${index + 1}. ${tarefa.nome} - ${if (tarefa.concluida) "Concluída" else "Não concluída"}")
+                }
+            }
+            3 -> {
+                // Conclusao de tarefa :p
+                print("Digite o número da tarefa para concluir: ")
+                val numTarefa = readLine()!!.toInt() - 1
+                if (numTarefa >= 0 && numTarefa < tarefas.size) {
+                    tarefas[numTarefa].concluida = true
+                    println("Tarefa concluída com sucesso!")
+                } else {
+                    println("Tarefa não encontrada!")
+                }
+            }
+            4 -> {
+                // Sair do programa
+                println("Saindo do programa...")
+                return
+            }
+            else -> {
+                println("Opção inválida!")
+            }
+        }
+    }
+}
